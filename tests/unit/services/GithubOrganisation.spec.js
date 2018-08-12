0d8ae0b83bb40f62f68a82d0584de5ae2eae73ca
Daniel Camina-Insua <d.camina91@gmail.com>
2018-08-12 17:45:18 +0100

    Tests written for the get issue fn

--- tests/unit/services/GithubOrganisation.spec.js
+++ tests/unit/services/GithubOrganisation.spec.js
@@ -9,27 +9,57 @@ describe('GitHub Organisation service', () => {
       expect(() => new GithubOrganisation()).toThrowError(expectedError);
     });
   });
-  describe('getRepositories Method', () => {
+  describe('Vaild Class instatiation', () => {
     const testClass = new GithubOrganisation('sample');
-    it('should retrive all avaible repos if the request is vaild (exists on github)', async () => {
-      const expectedResult = { Id: '123', full_name: 'nodejs/http-parser' };
-      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
-        ok: true,
-        json: () => Promise.resolve(expectedResult),
-      }));
-      const data = await testClass.getRepositories();
-      expect(data).toEqual(expectedResult);
+    describe('getRepositories Method', () => {
+      it('should retrive all avaible repos if the request is vaild (exists on github)', async () => {
+        const expectedResult = { Id: '123', full_name: 'nodejs/http-parser' };
+        window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(expectedResult),
+        }));
+        const data = await testClass.getRepositories();
+        expect(data).toEqual(expectedResult);
+      });
+      it('should resolve to a error if the request is invaild (github issues)', async () => {
+        window.fetch = jest.fn().mockImplementation(() => Promise.resolve(({
+          ok: false,
+          statusText: 'sample error',
+        })));
+        await expect(testClass.getRepositories()).rejects.toEqual(new Error('sample error'));
+      });
+      it('should resolve to a error if the request is invaild (internal issue eg no network)', async () => {
+        window.fetch = jest.fn().mockImplementation(() => Promise.reject(new Error('sample error')));
+        await expect(testClass.getRepositories()).rejects.toEqual(new Error('sample error'));
+      });
     });
-    it('should resolve to a error if the request is invaild (github issues)', async () => {
-      window.fetch = jest.fn().mockImplementation(() => Promise.resolve(({
-        ok: false,
-        statusText: 'sample error',
-      })));
-      await expect(testClass.getRepositories()).rejects.toEqual(new Error('sample error'));
-    });
-    it('should resolve to a error if the request is invaild (internal issue eg no network)', async () => {
-      window.fetch = jest.fn().mockImplementation(() => Promise.reject(new Error('sample error')));
-      await expect(testClass.getRepositories()).rejects.toEqual(new Error('sample error'));
+    describe('getRepositoryIssues Method', () => {
+      it('should resolve to a type error given a non/blank string argument', async () => {
+        const expected = new TypeError('Fn requires a non-empty string');
+        await expect(testClass.getRepositoryIssues(false)).rejects.toEqual(expected);
+        await expect(testClass.getRepositoryIssues('')).rejects.toEqual(expected);
+        await expect(testClass.getRepositoryIssues()).rejects.toEqual(expected);
+      });
+      it('should retrive all avaible repos if the request is vaild (exists on github)', async () => {
+        const expectedResult = [{ Id: '123', full_name: 'nodejs/http-parser' }];
+        window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(expectedResult),
+        }));
+        const data = await testClass.getRepositoryIssues('sample');
+        expect(data).toEqual(expectedResult);
+      });
+      it('should resolve to a error if the request is invaild (github issues)', async () => {
+        window.fetch = jest.fn().mockImplementation(() => Promise.resolve(({
+          ok: false,
+          statusText: 'not-found',
+        })));
+        await expect(testClass.getRepositoryIssues('sample')).rejects.toEqual(new Error('not-found'));
+      });
+      it('should resolve to a error if the request is invaild (internal issue eg no network)', async () => {
+        window.fetch = jest.fn().mockImplementation(() => Promise.reject(new Error('sample error')));
+        await expect(testClass.getRepositoryIssues('sample')).rejects.toEqual(new Error('sample error'));
+      });
     });
   });
 });
