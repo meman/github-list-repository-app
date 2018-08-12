c0cc7b143c45f1cf112efde3ad47619a1228f4ef
Daniel Camina-Insua <d.camina91@gmail.com>
2018-08-12 17:37:57 +0100

    Added getRepos tests

--- tests/unit/services/GithubOrganisation.spec.js
+++ tests/unit/services/GithubOrganisation.spec.js
@@ -9,4 +9,27 @@ describe('GitHub Organisation service', () => {
       expect(() => new GithubOrganisation()).toThrowError(expectedError);
     });
   });
+  describe('getRepositories Method', () => {
+    const testClass = new GithubOrganisation('sample');
+    it('should retrive all avaible repos if the request is vaild (exists on github)', async () => {
+      const expectedResult = { Id: '123', full_name: 'nodejs/http-parser' };
+      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(expectedResult),
+      }));
+      const data = await testClass.getRepositories();
+      expect(data).toEqual(expectedResult);
+    });
+    it('should resolve to a error if the request is invaild (github issues)', async () => {
+      window.fetch = jest.fn().mockImplementation(() => Promise.resolve(({
+        ok: false,
+        statusText: 'sample error',
+      })));
+      await expect(testClass.getRepositories()).rejects.toEqual(new Error('sample error'));
+    });
+    it('should resolve to a error if the request is invaild (internal issue eg no network)', async () => {
+      window.fetch = jest.fn().mockImplementation(() => Promise.reject(new Error('sample error')));
+      await expect(testClass.getRepositories()).rejects.toEqual(new Error('sample error'));
+    });
+  });
 });
